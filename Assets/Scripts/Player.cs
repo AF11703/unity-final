13e5b8ae7c7d41f96a59fb71571ff996d337933d
Andres Fraga <afraga2021@fau.edu>
2025-04-09 16:32:00 -0400

    Started refactor of Player script and adjusted animation clip parameters

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Animancer;
+using System.Collections;
+
+
+
+
+
+
+/*
+ !NOT DONE!
+
+Look at Unity's Input System guides on YouTube in order to call functions and play animations based on input.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 public class Player : _Character
 {
@@ -12,6 +37,9 @@ public class Player : _Character
     [SerializeField] AnimationClip swing2;
     [SerializeField] AnimationClip block;
 
+
+    Animator handAnimator;
+
     public enum PlayerState
     {
         Idle,
@@ -20,70 +48,78 @@ public class Player : _Character
         Jumping
     }
 
-    PlayerState currentState = PlayerState.Idle;    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+
+    PlayerState currentState = PlayerState.Idle;
+
+    PlayerInput inputActions;
+    private void Awake()
     {
-           
+        handAnimator = animancerComponent.Animator;
+        inputActions = GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-       if (!Keyboard.current.anyKey.isPressed)
-            currentState = PlayerState.Idle;
-       
-
-       if (Keyboard.current.spaceKey.isPressed)
-            currentState = PlayerState.Jumping;
-
-       if (Mouse.current.leftButton.isPressed)
-            currentState = PlayerState.Swinging;
-
-
-       if (Mouse.current.rightButton.isPressed)
-            currentState = PlayerState.Blocking;
-           
-
-
-        PlayAnimation(animancerComponent, currentState);
+        
     }
 
-    public void Heal(float healAmount) { 
-       base.setHealth(base.getHealth() + healAmount);
-    }
-
-    public void PlayAnimation(AnimancerComponent anim, PlayerState state, float time = 0, float normTime = 0f, float speed = 1f)
+ 
+    void PlayAnimation(AnimancerComponent ac, Animator an, PlayerState state)
     {
-        AnimationClip[] attackAnims = { swing1, swing2 };
-        bool alreadySwinging = anim.IsPlayingClip(attackAnims[0]) || anim.IsPlayingClip(attackAnims[1]);
+        if (ac.IsPlaying() && state != PlayerState.Idle)
+        {
+            return;
+        }
+
+        AnimationClip[] attackClips = { swing1, swing2 };
+
         switch (state)
         {
             case PlayerState.Idle:
-                if (alreadySwinging)
-                    return;
-                anim.Play(idleAnimation);
+                an.SetBool("Idle", true);
+                an.SetBool("Attack1", false);
+                an.SetBool("Attack2", false);
+                an.SetBool("Blocking", false);
+                an.SetBool("Block", false);
+
+                ac.Play(idleAnimation);
                 break;
+
             case PlayerState.Swinging:
-                if (alreadySwinging)
-                    return;
-                
-                // Randomly choose one of the attack animations
-                var randomIndex = Random.Range(0, attackAnims.Length);
-                
-                if (anim.IsPlayingClip(attackAnims[0]) || anim.IsPlayingClip(attackAnims[1]))
+                int randomIndex = Random.Range(0, attackClips.Length);
+                AnimationClip attackClip = attackClips[randomIndex];
+
+                an.SetBool("Idle", false);
+                an.SetBool("Blocking", false);
+                an.SetBool("Block", false);
+
+                if (attackClip == swing1)
                 {
                     
-                    // If an attack animation is already playing, return
-                    return;
+                    an.SetBool("Attack1", true);
+                    an.SetBool("Attack2", false);
+                   
                 }
-  
-                anim.Play(attackAnims[randomIndex]);
+                else 
+                {
+                    an.SetBool("Attack2", true);
+                    an.SetBool("Attack1", false);
+                }
+
+                ac.Play(attackClip);
                 break;
-            
+
             case PlayerState.Blocking:
-                anim.Play(block);
+                an.SetBool("Idle", false);
+                an.SetBool("Attack1", false);
+                an.SetBool("Attack2", false);
+                an.SetBool("Blocking", false);
+                an.SetBool("Block", true);
+
+                ac.Play(block);
                 break;
         }
+
     }
 }
